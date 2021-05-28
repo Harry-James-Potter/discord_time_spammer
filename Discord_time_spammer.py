3cd6f95c7aeeb7608ae24207bcde3c269656094e
Harry James Potter <77753224+Harry-James-Potter@users.noreply.github.com>
2021-05-28 19:50:28 +0530

    Update Discord_time_spammer.py

--- Discord_time_spammer.py
+++ Discord_time_spammer.py
@@ -1,18 +1,27 @@
 import time
 from pynput import keyboard
-keyboardd = keyboard.Controller()
 
-print (time.time)
+#  VARIABLES
+keyboardd = keyboard.Controller()
+print(time.time())
+
+#  TYPE FUNC
+def tpe(p_n):
+    keyboardd.type(str(ti_me.tm_hour - p_n) * 2)
+    keyboardd.press(keyboard.Key.enter)
+    keyboardd.release(keyboard.Key.enter)
+    time.sleep(600)
+    print(str(ti_me.tm_hour - p_n) * 2)
+
+#  WHILE LOOP WHICH REPEATS 1.25 TIMES EVERY MIN
 while True:
     ti_me = time.localtime(round(time.time()))
     if ti_me.tm_hour == ti_me.tm_min:
-        keyboard.type(str(ti_me.tm_hour)*2)
-        keyboardd.press(keyboard.Key.space)
-        keyboardd.release(keyboard.Key.space)
-        time.sleep(600)
+        tpe(0)
+    elif ti_me.tm_hour - 12 == ti_me.tm_min:
+        tpe(12)
+    time.sleep(45)
 
-    elif ti_me.tm_hour + 12 == ti_me.tm_min:
-        keyboardd.type(str(ti_me.tm_hour + 12)*2)
-        keyboardd.press(keyboard.Key.space)
-        keyboardd.release(keyboard.Key.space)
-        time.sleep(600) 
+    #  TO PREVENT FROM SLEEP
+    keyboardd.press(keyboard.Key.space)
+    keyboardd.release(keyboard.Key.space)
